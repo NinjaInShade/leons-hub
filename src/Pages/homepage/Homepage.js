d267c9e9e8e1912b1371296b7ee0f0650aae103f
Leon Michalak <leonmichalak6@gmail.com>
2020-11-21 15:45:57 +0000

    Implemented react spring to create a introductory animation for the homepage elements

--- src/Pages/homepage/Homepage.js
+++ src/Pages/homepage/Homepage.js
@@ -1,5 +1,6 @@
 // Libraries
 import React from "react";
+import { useSpring, animated, config } from "react-spring";
 
 // CSS
 import "../.././css/Homepage.css";
@@ -13,46 +14,55 @@ import Blob3 from "../../resources/svgs/Blob3.svg";
 import Card from "../.././components/layout/Card";
 
 export default function Homepage() {
+  const fromLeft = useSpring({ left: "0vw", from: { left: "-40vw" }, config: { ...config.default } });
+  const fromTop = useSpring({ top: "0vh", from: { top: "-10vh" }, config: { ...config.default } });
+
   return (
     <div className="homepage">
-      <header className="heading">
+      <animated.header className="heading" style={fromTop}>
         <h1>Leon's Portal</h1>
         <hr />
-      </header>
+      </animated.header>
       <section className="navigation">
-        <div className="blob-container">
+        <animated.div className="blob-container" style={fromLeft}>
           <Card>
             <i className="fab fa-wikipedia-w"></i>
             <h2>Wikipedia</h2>
           </Card>
           <img src={Blob3} alt="blob" className="blob blob3" />
-        </div>
-        <Card>
-          <span className="iconify" data-inline="false" data-icon="ri:calendar-todo-fill"></span>
-          <h2>To-Do</h2>
-        </Card>
-        <div className="blob-container">
+        </animated.div>
+        <animated.div style={{ ...fromLeft, position: "relative" }}>
+          <Card>
+            <span className="iconify" data-inline="false" data-icon="ri:calendar-todo-fill"></span>
+            <h2>To-Do</h2>
+          </Card>
+        </animated.div>
+        <animated.div className="blob-container" style={fromLeft}>
           <Card>
             <i className="fas fa-info-circle"></i>
             <h2>About</h2>
           </Card>
           <img src={Blob1} alt="blob" className="blob blob1" />
-        </div>
-        <Card>
-          <i className="fas fa-laptop-code"></i>
-          <h2>Code</h2>
-        </Card>
-        <div className="blob-container">
+        </animated.div>
+        <animated.div style={{ ...fromLeft, position: "relative" }}>
+          <Card>
+            <i className="fas fa-laptop-code"></i>
+            <h2>Code</h2>
+          </Card>
+        </animated.div>
+        <animated.div className="blob-container" style={fromLeft}>
           <Card>
             <i className="fas fa-book"></i>
             <h2>Books</h2>
           </Card>
           <img src={Blob2} alt="blob" className="blob blob2" />
-        </div>
-        <Card>
-          <i className="fas fa-cog"></i>
-          <h2>Settings</h2>
-        </Card>
+        </animated.div>
+        <animated.div style={{ ...fromLeft, position: "relative" }}>
+          <Card>
+            <i className="fas fa-cog"></i>
+            <h2>Settings</h2>
+          </Card>
+        </animated.div>
       </section>
     </div>
   );
