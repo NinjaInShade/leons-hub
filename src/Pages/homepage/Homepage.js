78ee80b31c2ae81fbde8db0277d2fce204995d92
Leon Michalak <leonmichalak6@gmail.com>
2021-03-11 19:19:59 +0000

    homepage refactored

--- src/Pages/homepage/Homepage.js
+++ src/Pages/homepage/Homepage.js
@@ -22,46 +22,54 @@ export default function Homepage() {
         <div className="blob-container">
           <img src={Blob3} alt="blob" className="blob blob3" />
           <a href="https://sitebuilder-66d12.web.app/" target="_blank" rel="noreferrer">
-            <Card>
-              <span class="iconify" data-inline="false" data-icon="ic:baseline-build"></span>
-              <h2>Site builder</h2>
+            <Card className="navigation-card">
+              <span
+                className="iconify navigation-icon"
+                data-inline="false"
+                data-icon="ic:baseline-build"
+              ></span>
+              <h2 className="navigation-text">Site builder</h2>
             </Card>
           </a>
         </div>
         <a href="https://todoapp-48ccc.web.app/" rel="noreferrer" target="_blank">
-          <Card>
-            <span className="iconify" data-inline="false" data-icon="ri:calendar-todo-fill"></span>
-            <h2>To-Do</h2>
+          <Card className="navigation-card">
+            <span
+              className="iconify navigation-icon"
+              data-inline="false"
+              data-icon="ri:calendar-todo-fill"
+            ></span>
+            <h2 className="navigation-text">To-Do</h2>
           </Card>
         </a>
         <div className="blob-container">
           <Link to="/About">
-            <Card>
-              <i className="fas fa-info-circle"></i>
-              <h2>About</h2>
+            <Card className="navigation-card">
+              <i className="fas fa-info-circle navigation-icon"></i>
+              <h2 className="navigation-text">About</h2>
             </Card>
           </Link>
           <img src={Blob1} alt="blob" className="blob blob1" />
         </div>
         <Link to="/Code">
-          <Card>
-            <i className="fas fa-laptop-code"></i>
-            <h2>Code</h2>
+          <Card className="navigation-card">
+            <i className="fas fa-laptop-code navigation-icon"></i>
+            <h2 className="navigation-text">Code</h2>
           </Card>
         </Link>
         <div className="blob-container">
           <Link to="/Books">
-            <Card>
-              <i className="fas fa-book"></i>
-              <h2>Books</h2>
+            <Card className="navigation-card">
+              <i className="fas fa-book navigation-icon"></i>
+              <h2 className="navigation-text">Books</h2>
             </Card>
           </Link>
           <img src={Blob2} alt="blob" className="blob blob2" />
         </div>
         <Link to="/Settings">
-          <Card>
-            <i className="fas fa-cog"></i>
-            <h2>Settings</h2>
+          <Card className="navigation-card">
+            <i className="fas fa-cog navigation-icon"></i>
+            <h2 className="navigation-text">Settings</h2>
           </Card>
         </Link>
       </animated.section>
